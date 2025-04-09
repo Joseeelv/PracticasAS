e2657c1fc86c86c0e9f0a3bdc8341a919e42bf18
Joseelv_ <jose.venegasan@gmail.com>
2025-04-09 13:01:15 +0200

    Void

--- backup_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# PGPASSWORD=$POSTGRES_PASSWORD pg_dump -U postgres postgres | gzip > /tmp/postgres-backup-$(date +%Y-%m-%d).sql.gz
-touch /tmp/prueba.txt
-RSYNC_PASSWORD=root rsync -avz --progress /tmp/prueba.txt rsync://root@172.20.0.2:873/backup
-
